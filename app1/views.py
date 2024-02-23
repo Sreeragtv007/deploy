05df51cfbbdd790baf8345101b8aa9ce97a878db
sreerag <sreeragtv007@gmail.com>
2024-02-23 15:50:39 +0530

    deploy

--- app1/views.py
+++ app1/views.py
@@ -186,8 +186,7 @@ def buyProduct(request, pk):
         pincode = request.POST.get('pincode')
         qty = request.POST.get('qty')
         productname = product.name
-      
-    
+
         buyproduct = Buyproduct.objects.create(
             user=request.user, product=product, qty=qty, address=address, pincode=pincode)
         messages.info(request, "you purchase request is sucessfully created")
@@ -201,10 +200,9 @@ def buyProduct(request, pk):
         # )
         # print('working')
         # print(type(totalprice))
-        total=int(qty)*product.price*100
-        
+        total = int(qty)*product.price*100
 
-        return redirect('index1',pk=total)
+        return redirect('index1', pk=total)
 
     context = {'product': product}
 
@@ -311,75 +309,76 @@ razorpay_client = razorpay.Client(
     auth=(settings.RAZOR_KEY_ID, settings.RAZOR_KEY_SECRET))
 
 
-
-def homepage(request,pk):
+def homepage(request, pk):
     currency = 'INR'
     amount = pk  # Rs. 200
- 
+
     # Create a Razorpay Order
     razorpay_order = razorpay_client.order.create(dict(amount=amount,
                                                        currency=currency,
                                                        payment_capture='0'))
- 
+
     # order id of newly created order.
     razorpay_order_id = razorpay_order['id']
     callback_url = 'paymenthandler/'
- 
+
     # we need to pass these details to frontend.
     context = {}
     context['razorpay_order_id'] = razorpay_order_id
     context['razorpay_merchant_key'] = settings.RAZOR_KEY_ID
     context['razorpay_amount'] = amount
+    context['razorpay_total_amount'] = amount/100
+
     context['currency'] = currency
     context['callback_url'] = callback_url
- 
+
     return render(request, 'index1.html', context=context)
- 
- 
+
+
 # we need to csrf_exempt this url as
 # POST request will be made by Razorpay
 # and it won't have the csrf token.
 
 def paymenthandler(request):
- 
+
     # only accept POST request.
     if request.method == "POST":
         try:
-           
+
             # get the required parameters from post request.
             payment_id = request.POST.get('razorpay_payment_id', '')
             razorpay_order_id = request.POST.get('razorpay_order_id', '')
             signature = request.POST.get('razorpay_signature', '')
-            amount=request.post.get('razorpay_amount','')
+            amount = request.post.get('razorpay_amount', '')
             params_dict = {
                 'razorpay_order_id': razorpay_order_id,
                 'razorpay_payment_id': payment_id,
                 'razorpay_signature': signature
             }
- 
+
             # verify the payment signature.
             result = razorpay_client.utility.verify_payment_signature(
                 params_dict)
             if result is not None:
-                
-                amount = amount  # Rs. 200
+
+                amount = amount
                 try:
- 
+
                     # capture the payemt
                     razorpay_client.payment.capture(payment_id, amount)
- 
+
                     # render success page on successful caputre of payment
                     return render(request, 'paymentsuccess.html')
                 except:
- 
+
                     # if there is an error while capturing payment.
                     return render(request, 'paymentfail.html')
             else:
- 
+
                 # if signature verification fails.
                 return render(request, 'paymentfail.html')
         except:
- 
+
             # if we don't find the required parameters in POST data
             return HttpResponseBadRequest()
     else:
