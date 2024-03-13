7d8c566f7b3623df1df439407aa12172189e1f48
sreerag <sreeragtv007@gmail.com>
2024-03-13 10:03:55 +0530

    data base changed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -80,21 +80,21 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'database-1',
-        'USER': 'postgres',
+        'NAME': 'ecommerce',
+        'USER': 'database-2',
         'PASSWORD': 'dilnatv2000',
-        'HOST': 'database-1.ctcyc8esk3si.ap-southeast-2.rds.amazonaws.com',
+        'HOST': 'database-2.ctcyc8esk3si.ap-southeast-2.rds.amazonaws.com',
         'PORT': '5432'
     }
 }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
