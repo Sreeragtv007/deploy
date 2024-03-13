19a2a6cdd377d4d991c284cf2f13a1ec84e9d767
sreerag <sreeragtv007@gmail.com>
2024-03-13 10:13:50 +0530

    database sql lite

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce',
-        'USER': 'database-2',
-        'PASSWORD': 'dilnatv2000',
-        'HOST': 'database-2.ctcyc8esk3si.ap-southeast-2.rds.amazonaws.com',
-        'PORT': '5432'
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ecommerce',
+#         'USER': 'database-2',
+#         'PASSWORD': 'dilnatv2000',
+#         'HOST': 'database-2.ctcyc8esk3si.ap-southeast-2.rds.amazonaws.com',
+#         'PORT': '5432'
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
