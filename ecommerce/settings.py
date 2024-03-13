6ac8d74a618afe487c86d8021802e33fbcbc58c2
sreerag <sreeragtv007@gmail.com>
2024-03-13 09:53:08 +0530

    deploy

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -77,16 +77,16 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'database-1',
-#         'USER': 'postgres',
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database-1',
+        'USER': 'postgres',
+        'PASSWORD': 'dilnatv2000',
+        'HOST': 'database-1.ctcyc8esk3si.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
 
 
 DATABASES = {
